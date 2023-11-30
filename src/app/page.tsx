b35d0e917676ce45d9bd3ae36bf587e193156b30
ThiagoJoseSousa <imigrationstudy@gmail.com>
2023-11-30 00:33:50 -0300

    Updated images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@ import Image from "next/image"
 export default function Home() {
   return (
     <main>
-      <section id="Hero" className="h-screen bg-image-before bg-image-after relative">
-        <Image src="/Socrates-back.webp" className="absolute block h-screen object-cover" priority alt="Hero background"  width={1920} height={1097}/>
-        <h1 className="absolute">Socrates</h1>
-        <Image src="/Socrates-front.webp" className="absolute block h-screen object-cover" priority alt="Hero Socrates"  width={1920} height={1097}/>
-        <Image src="/Socrates-mobile.webp" className="absolute block h-screen object-cover object-right" alt="Socrates receives a notification." width={1792} height={2304}/>
-
+      <section id="Hero" className="h-screen bg-image-before bg-image-after">
+        <Image src="/Socrates-desktop.webp" className="hidden md:block h-screen object-cover" priority alt="Hero background"  width={2688} height={1536}/>
+        <h1 className="absolute ">Socrates</h1>
+        <Image src="/Socrates-mobile.webp" className="block md:hidden h-screen object-cover object-right" alt="Socrates receives a notification." width={1792} height={2304}/>
       </section>
     </main>
   )
