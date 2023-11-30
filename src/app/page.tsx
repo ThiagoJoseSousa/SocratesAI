88618ccf9ad40fcdd47329f5f134a5bf0f5ccd9c
ThiagoJoseSousa <imigrationstudy@gmail.com>
2023-11-29 23:08:39 -0300

    Created two images, one 3:4 and other 16:9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section id="Hero">
-        AA
+    <main>
+      <section id="Hero" className="h-screen bg-image-before bg-image-after relative">
+        <Image src="/Socrates-back.webp" className="absolute block h-screen object-cover" priority alt="Hero background"  width={1920} height={1097}/>
+        <h1 className="absolute">Socrates</h1>
+        <Image src="/Socrates-front.webp" className="absolute block h-screen object-cover" priority alt="Hero Socrates"  width={1920} height={1097}/>
+        <Image src="/Socrates-mobile.webp" className="absolute block h-screen object-cover object-right" alt="Socrates receives a notification." width={1792} height={2304}/>
+
       </section>
     </main>
   )
